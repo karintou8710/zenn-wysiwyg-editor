351448fbc4aeea48cdcca5bb1eeb4e2ce06b8b69
karintou8710 <karintou8710@gmail.com>
2025-08-13 22:45:26 +0900

    feat: Heading

--- src/features/editor/editor.tsx
+++ src/features/editor/editor.tsx
@@ -7,10 +7,18 @@ import Text from "@tiptap/extension-text";
 // @ts-ignore
 import "zenn-content-css";
 import "./editor.css";
+import Heading from "./extensions/heading";
 
 function Editor() {
   const editor = useEditor({
-    extensions: [Document, Paragraph, Text],
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      Heading.configure({
+        levels: [1, 2, 3, 4],
+      }),
+    ],
     content: "<p>Hello World!</p>",
   });
 
---
+++ src/features/editor/extensions/heading/index.tsx
@@ -0,0 +1,35 @@
+import { Heading as TiptapHeading } from "@tiptap/extension-heading";
+import { splitBlockAs } from "@tiptap/pm/commands";
+
+const Heading = TiptapHeading.extend({
+  addKeyboardShortcuts() {
+    return {
+      // @ts-ignore: https://github.com/ueberdosis/tiptap/issues/6670
+      ...this.parent?.(),
+
+      Backspace: () => {
+        const { selection } = this.editor.state;
+        const { $from } = selection;
+        if ($from.node().type.name !== this.name) return false;
+
+        if (!selection.empty || $from.start() !== $from.pos) return false;
+
+        return this.editor.commands.setParagraph();
+      },
+      Enter: () => {
+        const { selection } = this.editor.state;
+        const { $from } = selection;
+        if ($from.node().type.name !== this.name) return false;
+        if ($from.start() === $from.pos) return false;
+
+        return splitBlockAs(() => {
+          return {
+            type: this.editor.schema.nodes.paragraph,
+          };
+        })(this.editor.state, this.editor.view.dispatch);
+      },
+    };
+  },
+});
+
+export default Heading;
