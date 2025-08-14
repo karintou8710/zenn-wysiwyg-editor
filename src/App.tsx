c1896150bf227a45b500808d0def4026b8c83e54
karintou8710 <karintou8710@gmail.com>
2025-08-14 15:52:44 +0900

    fix: headingのfont-weightを修正

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Editor from "./features/editor/editor";
 
 function App() {
   return (
-    <div className="mt-20 max-w-[800px] px-10 mx-auto">
+    <div className="mt-20 max-w-[800px] mx-auto">
       <Editor />
     </div>
   );
