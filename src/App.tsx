afd977ce8851bd84db54c799baccff5108e892ca
karintou8710 <karintou8710@gmail.com>
2025-08-15 20:13:34 +0900

    feat: 警告文を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
 import Editor from "./features/editor/editor";
 
 function App() {
   return (
-    <div className="mt-20 max-w-[800px] mx-auto">
+    <div className="mt-10 max-w-[800px] mx-auto">
+      <div className="mb-6">
+        <Alert variant="destructive">
+          <AlertDescription>
+            現在は開発途中なため、アップデートにより内部構造が崩れてしまう可能性があります。
+            <br />
+            大事な文書は書き終えた後にコピーを取ることをお勧めします。
+          </AlertDescription>
+        </Alert>
+      </div>
       <Editor />
     </div>
   );
