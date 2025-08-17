b259357262dff5f3d0ae563882bb86b81d47bbcb
karintou8710 <karintou8710@gmail.com>
2025-08-18 04:24:47 +0900

    fix: ファイル名ノードを強制追加

--- src/features/editor/lib/from_markdown.ts
+++ src/features/editor/lib/from_markdown.ts
@@ -6,6 +6,7 @@ export function fromMarkdown(text: string) {
   dom.innerHTML = html;
 
   removeMessageSymbol(dom);
+  addCodeBlockFileName(dom);
 
   return dom.innerHTML;
 }
@@ -14,5 +15,20 @@ function removeMessageSymbol(dom: HTMLElement) {
   const messageSymbols = dom.querySelectorAll(".msg-symbol");
   messageSymbols.forEach((el) => {
     el.remove();
+  });
+}
+
+function addCodeBlockFileName(dom: HTMLElement) {
+  const codeBlockContainers = dom.querySelectorAll(".code-block-container");
+  codeBlockContainers.forEach((codeBlockContainer) => {
+    if (codeBlockContainer.children.length === 1) {
+      // ファイル名がないので追加する
+      const fileNameDom = document.createElement("div");
+      fileNameDom.className = "code-block-filename-container";
+      codeBlockContainer.insertBefore(
+        fileNameDom,
+        codeBlockContainer.firstChild
+      );
+    }
   });
 }
