fdaed700fc2fc94ef1c9c1377ce037defced6d33
karintou8710 <karintou8710@gmail.com>
2025-08-25 01:43:10 +0900

    refactor: ケバブケースに

--- src/features/editor/components/fixed-menu/index.tsx
+++ src/features/editor/components/fixed-menu/index.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { showToast } from "@/lib/toast";
 import { cn } from "@/lib/utils";
 import { INITIAL_CONTENT } from "../../lib/initial-content";
-import { markdownSerializer } from "../../lib/to_markdown";
+import { markdownSerializer } from "../../lib/to-markdown";
 import MarkdownPasteDialog from "../markdown-paste-dialog";
 
 type Props = {
