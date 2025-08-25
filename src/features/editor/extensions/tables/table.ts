897fc9cc73976df9f75967609a4d44f62a0b9038
karintou8710 <karintou8710@gmail.com>
2025-08-25 22:27:18 +0900

    fix: 行数列数のバリデーション

--- src/features/editor/extensions/tables/table.ts
+++ src/features/editor/extensions/tables/table.ts
@@ -5,10 +5,14 @@ export const Table = TiptapTable.extend({
   addInputRules() {
     return [
       new InputRule({
-        find: /^:::table(\d+)-(\d+)\s$/,
+        find: /^:::table(\d{1,2})-(\d{1,2})\s$/,
         handler: ({ match, range, chain }) => {
           const rows = parseInt(match[1], 10);
           const columns = parseInt(match[2], 10);
+
+          if (rows === 0 || columns === 0 || rows > 20 || columns > 20) {
+            return;
+          }
 
           chain()
             .deleteRange({ from: range.from, to: range.to })
