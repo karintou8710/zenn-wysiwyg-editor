8ba117f0783658f7b17dc47a990e50eaffa59630
karintou8710 <karintou8710@gmail.com>
2025-08-25 17:26:22 +0900

    fix: 脚注を跨ぐトランザクションの制御

--- src/features/editor/extensions/footnotes/footnotes.ts
+++ src/features/editor/extensions/footnotes/footnotes.ts
@@ -37,6 +37,30 @@ const Footnotes = Node.create({
     return [
       new Plugin({
         key: new PluginKey("footnoteRules"),
+        filterTransaction(tr) {
+          const { from, to } = tr.selection;
+
+          // 全選択の場合は許可
+          if (from === 0 && to === tr.doc.content.size) return true;
+
+          let selectedFootnotes = false;
+          let selectedContent = false;
+          let footnoteCount = 0;
+
+          tr.doc.nodesBetween(from, to, (node, _, parent) => {
+            if (parent?.type.name === "doc" && node.type.name !== "footnotes") {
+              selectedContent = true;
+            } else if (node.type.name === "footnoteItem") {
+              footnoteCount += 1;
+            } else if (node.type.name === "footnotes") {
+              selectedFootnotes = true;
+            }
+          });
+          const overSelected = selectedContent && selectedFootnotes;
+
+          // コンテンツと脚注を跨ぐ or 脚注アイテムを跨ぐ トランザクションは不可
+          return !overSelected && footnoteCount <= 1;
+        },
 
         // 脚注参照が追加・削除・移動時に、脚注関連を更新する
         appendTransaction(transactions, _, newState) {
