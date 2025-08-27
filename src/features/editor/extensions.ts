447b7e6acb9c0cf8ace2177ac05db07d84ebeb46
karintou8710 <karintou8710@gmail.com>
2025-08-27 19:13:07 +0900

    feat: CSSno

--- src/features/editor/extensions.ts
+++ src/features/editor/extensions.ts
@@ -17,7 +17,6 @@ import Text from "@tiptap/extension-text";
 import { Dropcursor, TrailingNode, UndoRedo } from "@tiptap/extensions";
 import { type Extensions, findParentNode } from "@tiptap/react";
 import { FileHandler } from "./extensions/functionality/file-handler";
-import { NodeSelectionRect } from "./extensions/functionality/node-selection-rect";
 import { Placeholder } from "./extensions/functionality/placeholder";
 import { Link } from "./extensions/marks/link";
 import { CodeBlockContainer } from "./extensions/nodes/code-block-container";
@@ -119,5 +118,4 @@ export const extensions: Extensions = [
   Dropcursor,
   FileHandler,
   ListKeymap,
-  NodeSelectionRect,
 ];
